722ac4e5b9072652a8c84e6f0de035f3a0175e66
masayuki0228 <hirosawa0228@icloud.com>
2022-03-23 20:02:02 +0900

    ✨ onChangeでsetStringに代入する。onClickでsetNameに代入する。

--- src/component/SignIn.jsx
+++ src/component/SignIn.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useState } from "react";
 
-export function SignIn() {
+export function SignIn(props) {
+  const [string, setString] = useState("");
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-24 px-4 sm:px-6 lg:px-8">
@@ -11,8 +13,8 @@ export function SignIn() {
           <form className="mt-8 space-y-6" action="#" method="POST">
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
+                <label htmlFor="name" className="sr-only">
+                  Name
                 </label>
                 <input
                   id="name"
@@ -21,14 +23,18 @@ export function SignIn() {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="ニックネーム"
+                  onChange={(e) => setString(e.target.value)}
                 />
               </div>
             </div>
 
             <div>
               <button
-                type="submit"
+                type="button"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={() => {
+                  props.setName(string);
+                }}
               >
                 はじめる
               </button>
