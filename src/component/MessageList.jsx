f9e40768f41ef4fa584eb65c99d2120462068ccd
masayuki0228 <hirosawa0228@icloud.com>
2022-03-27 10:39:05 +0900

    ✨ メッセージリストコンポーネントを追加し、メッセージ一覧を取得します。

--- src/component/MessageList.jsx
+++ src/component/MessageList.jsx
@@ -1,3 +1,22 @@
+import { messageRef } from "src/firebase";
+import { onValue } from "firebase/database";
+import { useState, useEffect } from "react";
+
 export function MessageList() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    onValue(messageRef, (snapshot) => {
+      const messages = snapshot.val();
+      if (messages === null) return;
+      const entries = Object.entries(messages);
+      const newMessages = entries.map((entry) => {
+        const [key, nameAndText] = entry;
+        return { key, ...nameAndText };
+      });
+      setMessages(newMessages);
+    });
+  }, []);
+  console.log(messages);
   return <div>MessageList</div>;
 }
