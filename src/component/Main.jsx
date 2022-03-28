cff445acf6701d0f577257f16af55b3ca394f364
masayuki0228 <hirosawa0228@icloud.com>
2022-03-29 00:26:10 +0900

    🐛

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,16 +1,28 @@
 import Head from "next/head";
+import styles from "src/styles/Home.module.css";
 import { MessageInputField } from "src/component/MessageInputField";
 import { MessageList } from "src/component/MessageList";
 
+function setHeight() {
+  if (typeof window !== "undefined") {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+}
+if (typeof window !== "undefined") {
+  setHeight();
+  window.addEventListener("resize", setHeight);
+}
 export function Main(props) {
   return (
     <div>
       <Head>
         <title>Main</title>
       </Head>
-      <div className="grid grid-rows-1 min-h-screen">
+      {/* <div className="grid grid-rows-1 min-h-screen"> */}
+      <div className={styles.main}>
         <MessageList />
-        <MessageInputField name={props.name}/>
+        <MessageInputField name={props.name} />
       </div>
     </div>
   );
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import "src/styles/globals.css";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
