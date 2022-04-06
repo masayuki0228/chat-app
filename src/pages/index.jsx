63cd40711fd752e2fce7b4e84dc70ad312dae5da
masayuki0228 <hirosawa0228@icloud.com>
2022-04-07 01:10:43 +0900

    indexにグローバルな値を持たせました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
 import { Main } from "src/component/Main";
+import { userState } from "src/store/userState";
 
 export default function Index() {
+  const name = useRecoilValue(userState);
+  const router = useRouter();
+  useEffect(() => {
+    if (name.name === "") {
+      router.push("/signIn");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return <Main />;
 }
